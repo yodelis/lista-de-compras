55a8509e8b9cd48e95f8d8e6cb0b6acb9476717a
yodelis <yaselcubesp@yahoo.es>
2016-12-20 16:22:52 +0100

    Ejercicio Lista de compra actualizada

--- main.js
+++ main.js
@@ -1,21 +1,42 @@
 $(document).ready(function(e) {
-  $("#btnañadir").click(function(event) {
-    var elemento = $("#elemento").val();
-    if (elemento == "") {
-      console.log("El elemento no existe");
-    } else {
-      $("li:last").after('<li class="list-group-item">' + elemento + '</li>');
-      $("#elemento").val('');
-    }
-    event.preventDefault();
-  });
+    $("#btnañadir").click(function(event) {
+        var elemento = $("#elemento").val();
+        if (elemento == "") {
+            console.log("El elemento no existe");
+        } else {
+            $("li:last").after('<li class="list-group-item">' + elemento + '</li>');
+            $("#elemento").val('');
+        }
+        event.preventDefault();
+    });
 
-  $("ul").on('click', 'li', function() {
-    $(this).toggleClass('active');
+    $("ul").on('click', 'li', function() {
+        var badge = $(this).children('.badge');
+        var contador = parseInt(badge.html());
+        if (contador == 0) {
+          badge.html(contador + 1);
+          $(this).addClass('active')
 
-    var badge = $(this).children('.badge');
-    var contador = parseInt(badge.html());
-    badge.html(contador + 1);
+        } else {
+          badge.html('0');
 
-  });
+        }
+        $(this).toggleClass('active');
+
+
+
+    });
+    $("ul").on('click', 'button', function(event) {
+        event.preventDefault();
+        var badge = $(this).siblings('.badge');
+        var contador = parseInt(badge.html());
+        if (contador == 0) {
+            $(this).parent('li').addClass('active')
+        }
+        badge.html(contador + 1);
+        
+        return false;
+
+    });
+
 });
